0d60724339653d5b1493ced87ff24add39af7603
Kunal Chandra <kunal.chandra1900@gmail.com>
2023-11-26 16:48:23 +0530

    added razorpay payment

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const app = express();
+const PORT = 5000;
+const Razorpay = require('razorpay');
+
+dotenv.config()
+
+app.use(express.json());
+
+app.use(cors({
+    origin: ['http://127.0.0.1:3000'],
+    credentials: true
+}));
+
+app.post('/placeOrder', (req, res, next) => {
+    try {
+        var instance = new Razorpay({
+            key_id: process.env.API_KEY,
+            key_secret: process.env.API_SECRET,
+        });
+
+        var options = {
+            amount: req.body.amount, // amount in the smallest currency unit
+            currency: "INR",
+            receipt: "order_rcptid_11"
+        };
+        instance.orders.create(options, function (err, order) {
+            console.log(order);
+
+            return res.status(200).json({
+                message: 'Order placed successfully!',
+                success: true,
+                status: 'success',
+                data: {
+                    order
+                }
+            })
+        });
+    } catch (error) {
+        console.log("Failed to place order");
+        return res.status(500), json({
+            message: 'Failed to place order, server error',
+            success: false,
+            status: 'fail',
+            error: error
+        })
+    }
+})
+
+app.listen(PORT, () => {
+    console.log('Server listening on port: ' + PORT);
+})
+
+// {
+//     id: 'order_N5582nQ9EOh8Ek',
+//     entity: 'order',
+//     amount: 5000,
+//     amount_paid: 0,
+//     amount_due: 5000,
+//     currency: 'INR',
+//     receipt: 'order_rcptid_11',
+//     offer_id: null,
+//     status: 'created',
+//     attempts: 0,
+//     notes: [],
+//     created_at: 1700997156
+//   }
